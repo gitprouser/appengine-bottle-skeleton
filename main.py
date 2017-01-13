d1bbe46a4c1aa48acf3d5b629c1b82623351b44f
Dhawan Gajendran Gayash <dgayash@paypal.com>
2017-01-12 19:55:46 -0800

    Adding payments and payer_id changes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bottle import request
 import requests
 import logging
 import time
+import main
 
 
 # Create the Bottle WSGI application.
@@ -15,12 +16,19 @@ bottle = Bottle()
 
 sandbox_url = 'https://api.sandbox.paypal.com'
 sandbox_web_url = 'https://www.sandbox.paypal.com'
+
+
 token_service = '/v1/oauth2/token'
 user_info_service = '/v1/oauth2/token/userinfo?schema=openid'
+payment_endpoint = '/v1/payments/payment'
+
+
 prepend_str = len('https://www.paypal.com/webapps/auth/identity/user/')
 
 # TODO Place holder to keep the {"user id" : "refresh tokens"}
 user_management = {}
+user_game_id = {}
+cached_game_id = ''
 
 redirect_url = 'https://hackathon-155402.appspot.com'
 lipp_url = sandbox_web_url + '/signin/authorize?response_type=code&scope=https%3A%2F%2Furi.paypal.com%2Fservices%2Fpayments%2Ffuturepayments&redirect_uri=https%3A%2F%2Fhackathon-155402.appspot.com&client_id=AfCEGMbuFg_FvwVqX5kOXR6H5mYMurdHwmXgikTBWe2HIbqehI6bni5TuFH3QFDf3I3HhXR_6xk8FF0r'
@@ -76,16 +84,29 @@ def hello():
     # payer_id - is the uniquie identifier
     payer_id = user_info_response['payer_id']
     user_management[payer_id] = refresh_token
-    return 'Hello World:' + user_info_response['payer_id']
+    logging.info("cached_game_id:'" + cached_game_id + "'")
+    user_game_id[cached_game_id] = payer_id
+    logging.info("user_game_id")
+    logging.info(user_game_id)
+    # return user_info_response['payer_id']
+    return "Authentication complete! Please close this window"
 
 
-@bottle.get('/<game_id:int>/login')
+@bottle.get('/<game_id>/get_payer_id')
+def get_payer_id(game_id):
+    logging.info('user_game_id:')
+    logging.info(user_game_id[game_id])
+    return user_game_id[game_id]
+
+
+@bottle.get('/<game_id>/login')
 def lipp(game_id):
     # do a get request : https://www.msmaster.qa.paypal.com/signin/authorize?client_id=AU9Brr7cKnWaL9uFiOlCnwgKaYDO3pa9nvOBhM6qJZCuxCnPMi3VdfPm45qkOrg6L_S5w2hlpmXnSX6V&response_type=code&scope=https://uri.paypal.com/services/payments/futurepayments&redirect_uri=urn:ietf:wg:oauth:2.0:oob&partner_client_id=AcnzQ3fr47rBqJmxFVpwSBOws7W8Elkk6fJBpF7pfsgG8_FBckr4NQEloEPhVBw3tS0elJ9Azm2jaoSO
     # response CODE
+    main.cached_game_id = game_id
     return lipp_url
 
-@bottle.get('/<payer_id>/refresh_token')
+@bottle.get('/<payer_id>/access_token')
 def refresh_token_exchange(payer_id):
     refresh_token = user_management[payer_id]
     access_token = rt_to_at_exchange(refresh_token)
@@ -114,6 +135,26 @@ def rt_to_at_exchange(refresh_token):
     return rt_to_at_resp['access_token']
 
 
+@bottle.post('/<payer_id>/payment')
+def payments(payer_id):
+    refresh_token = user_management[payer_id]
+    access_token = rt_to_at_exchange(refresh_token)
+    logging.info("access_token:'" + access_token + "'")
+
+    payment_headers = {'Content-Type':'application/json', 'Authorization' : 'Bearer ' + access_token}
+    logging.info('payment_headers' + str(payment_headers))
+
+    payload = request.POST.get('sale_request')
+    logging.info('payload')
+    logging.info(payload)
+    try:
+        r = requests.post(sandbox_url + payment_endpoint, headers=payment_headers, data=payload, verify=False)
+    except Exception as e:
+        logging.info(e)
+    payment_response = r.json()
+    logging.info("response headers" + (', '.join("%s=%r" % (key, val) for (key, val) in r.headers.iteritems())))
+    logging.info('payment_id' + payment_response['id'])
+    return payment_response['id']
 
 # Define an handler for 404 errors.
 @bottle.error(404)
